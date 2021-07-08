139f2c1b0114201acc051fe986ecdc7cb15af70a
Sergey Novikov <foxinazombobombo@mail.ru>
2021-07-08 20:13:55 +0300

    Update the "build/" directory
    The files in the "build /" directory
    have been updated.

---
+++ build/js/modal.js
@@ -0,0 +1,40 @@
+// --------- DOM-элементы ---------
+const body = document.querySelector('body');
+
+const modals = body.querySelectorAll('.modal');
+const callbackButton = body.querySelector('#callback-button');
+const callbackPopupCloseButton = body.querySelector('#callback-popup-close');
+
+
+/*
+=================================================================
+------------------------ ОСНОВНАЯ ЛОГИКА ------------------------
+=================================================================
+*/
+
+// *** Функция для обработчика события ОТКРЫТИЯ попапа с Формой обратной связи ***
+const onCallbackButtonClick = (evt) => {
+  evt.preventDefault();
+
+  for (let modalOpen of modals) {
+    modalOpen.classList.add('modal--show');
+  }
+
+  callbackPopupCloseButton.addEventListener('click', onCallbackPopupClose);
+};
+
+
+// *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой обратной связи ***
+const onCallbackPopupClose = (evt) => {
+  evt.preventDefault();
+
+  for (let modalClose of modals) {
+    modalClose.classList.remove('modal--show');
+  }
+
+  callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+};
+
+
+// === ДОБАВЛЕНИЕ обработчика события Открытия попапа с Формой ===
+callbackButton.addEventListener('click', onCallbackButtonClick);
