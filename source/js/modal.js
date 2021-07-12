5872923137d364fb22628b5aeabaf99b01b89275
Sergey Novikov <foxinazombobombo@mail.ru>
2021-07-12 13:17:06 +0300

    Wrap the script in IIFE
    The script code for the logic of the modal
    window was wrapped in IIFE, as required by
    the quality criteria.

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,40 +1,50 @@
-// --------- DOM-элементы ---------
-const body = document.querySelector('body');
-
-const modals = body.querySelectorAll('.modal');
-const callbackButton = body.querySelector('#callback-button');
-const callbackPopupCloseButton = body.querySelector('#callback-popup-close');
-
+'use strict';
 
 /*
-=================================================================
------------------------- ОСНОВНАЯ ЛОГИКА ------------------------
-=================================================================
+===================================================================================================================================
+------------------------------------------- Модуль логики работы Модального окна: НАЧАЛО ------------------------------------------
+===================================================================================================================================
 */
+window.modal = (() => {
+  // --------- DOM-элементы ---------
+  const body = document.querySelector('body');
 
-// *** Функция для обработчика события ОТКРЫТИЯ попапа с Формой обратной связи ***
-const onCallbackButtonClick = (evt) => {
-  evt.preventDefault();
-
-  for (let modalOpen of modals) {
-    modalOpen.classList.add('modal--show');
-  }
-
-  callbackPopupCloseButton.addEventListener('click', onCallbackPopupClose);
-};
+  const modals = body.querySelectorAll('.modal');
+  const callbackButton = body.querySelector('#callback-button');
+  const callbackPopupCloseButton = body.querySelector('#callback-popup-close');
 
 
-// *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой обратной связи ***
-const onCallbackPopupClose = (evt) => {
-  evt.preventDefault();
+  /*
+  ======================== ОСНОВНАЯ ЛОГИКА ========================
+  */
 
-  for (let modalClose of modals) {
-    modalClose.classList.remove('modal--show');
-  }
+  // *** Функция для обработчика события ОТКРЫТИЯ попапа с Формой обратной связи ***
+  const onCallbackButtonClick = (evt) => {
+    evt.preventDefault();
 
-  callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
-};
+    for (let modalOpen of modals) {
+      modalOpen.classList.add('modal--show');
+    }
+
+    callbackPopupCloseButton.addEventListener('click', onCallbackPopupClose);
+  };
 
 
-// === ДОБАВЛЕНИЕ обработчика события Открытия попапа с Формой ===
-callbackButton.addEventListener('click', onCallbackButtonClick);
+  // *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой обратной связи ***
+  const onCallbackPopupClose = (evt) => {
+    evt.preventDefault();
+
+    for (let modalClose of modals) {
+      modalClose.classList.remove('modal--show');
+    }
+
+    callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+  };
+
+
+  // === ДОБАВЛЕНИЕ обработчика события Открытия попапа с Формой ===
+  callbackButton.addEventListener('click', onCallbackButtonClick);
+})();
+/*
+*** --------------------------------------------- Модуль логики Модального окна: КОНЕЦ --------------------------------------------
+*/
