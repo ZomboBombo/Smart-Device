60989c275fe85c1a5ecdd83a74d4dca5181cdfdd
Sergey Novikov <foxinazombobombo@mail.ru>
2021-07-16 13:05:02 +0300

    Supplement the logic of the Modal
    New event handlers have been added to
    the logic of the modal window:
    ---
    
    * onEscPress() — event handler for
    Closing the modal window when the
    "Escape" key is pressed;
    
    * onModalOverlayClick() — event handler
    for Closing modal window when clicking
    on the overlay;
    
    Added settings to add these event handlers
    at the right time, as well as to remove
    them on time.
    
    Also added a little logic for focusing
    the cursor in the "Name" input field when
    opening a modal window.

--- source/js/modal.js
+++ source/js/modal.js
@@ -6,10 +6,20 @@
 ===================================================================================================================================
 */
 window.modal = (() => {
+  // ----------- КОНСТАНТЫ ----------
+  // *** Словарик для значений клавиши "Escape" ***
+  const EscapeKey = {
+    FULL_NAME: 'Escape',
+    ABBREVIATED_NAME: 'Esc',
+  };
+
+
   // --------- DOM-элементы ---------
   const body = document.querySelector('body');
 
   const modals = body.querySelectorAll('.modal');
+  const modalOverlay = body.querySelector('#modal-overlay');
+  const formNameField = modalOverlay.querySelector('#callback-popup-customer-name');
   const callbackButton = body.querySelector('#callback-button');
   const callbackPopupCloseButton = body.querySelector('#callback-popup-close');
 
@@ -17,6 +27,59 @@ window.modal = (() => {
   /*
   ======================== ОСНОВНАЯ ЛОГИКА ========================
   */
+
+  /*
+  *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой
+  *** при НАЖАТИИ на клавишу "Escape"
+  */
+  const onEscPress = (evt) => {
+    if (evt.key === EscapeKey.FULL_NAME || evt.key === EscapeKey.ABBREVIATED_NAME) {
+      evt.preventDefault();
+
+      for (let modalClose of modals) {
+        modalClose.classList.remove('modal--show');
+      }
+
+      callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+      modalOverlay.removeEventListener('click', onModalOverlayClick);
+      document.removeEventListener('keydown', onEscPress);
+    }
+  };
+
+
+  /*
+  *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой
+  *** при КЛИКЕ на "пустое пространство" (оверлей попапа)
+  */
+  const onModalOverlayClick = (evt) => {
+    if (evt.target === modalOverlay) {
+      for (let modalClose of modals) {
+        modalClose.classList.remove('modal--show');
+      }
+
+      callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+      modalOverlay.removeEventListener('click', onModalOverlayClick);
+      document.addEventListener('keydown', onEscPress);
+    }
+  };
+
+
+  /*
+  *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой
+  *** при КЛИКЕ на кнопку Закрытия ("крестик")
+  */
+  const onCallbackPopupClose = (evt) => {
+    evt.preventDefault();
+
+    for (let modalClose of modals) {
+      modalClose.classList.remove('modal--show');
+    }
+
+    callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+    modalOverlay.removeEventListener('click', onModalOverlayClick);
+    document.removeEventListener('keydown', onEscPress);
+  };
+
 
   // *** Функция для обработчика события ОТКРЫТИЯ попапа с Формой обратной связи ***
   const onCallbackButtonClick = (evt) => {
@@ -26,19 +89,11 @@ window.modal = (() => {
       modalOpen.classList.add('modal--show');
     }
 
+    formNameField.focus();
+
     callbackPopupCloseButton.addEventListener('click', onCallbackPopupClose);
-  };
-
-
-  // *** Функция для обработчика события ЗАКРЫТИЯ попапа с Формой обратной связи ***
-  const onCallbackPopupClose = (evt) => {
-    evt.preventDefault();
-
-    for (let modalClose of modals) {
-      modalClose.classList.remove('modal--show');
-    }
-
-    callbackPopupCloseButton.removeEventListener('click', onCallbackPopupClose);
+    modalOverlay.addEventListener('click', onModalOverlayClick);
+    document.addEventListener('keydown', onEscPress);
   };
 
 
