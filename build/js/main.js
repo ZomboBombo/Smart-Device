cc38bd455b2bbbbb114c013f883909c4de9f25d8
Sergey Novikov <foxinazombobombo@mail.ru>
2021-07-24 17:45:41 +0300

    Update files in the build/ directory

--- build/js/main.js
+++ build/js/main.js
@@ -171,6 +171,21 @@ window.maskForInputs = (() => {
   const MIN_PHONE_LENGHT = 11;
   const EMPTY = '';
 
+  // *** Словарь языковых аббревиатур ***
+  const LanguageAbbr = {
+    RU: 'ru',
+    ru_RU: 'ru-RU',
+    EN: 'en',
+    en_US: 'en-US',
+    en_GB: 'en-GB',
+  };
+
+  // *** Словарь сообщений валидации полей Формы ***
+  const ValidityMessage = {
+    PHONE_RU: 'Номер телефона введён не полностью. Пожалуйста, введите номер телефона.',
+    PHONE_EN: 'The phone number is incomplete. Please enter your phone number.',
+  };
+
 
   // --------- DOM-элементы ---------
   const telInputs = document.querySelectorAll('input[type="tel"]');
@@ -180,6 +195,28 @@ window.maskForInputs = (() => {
   ======================== ОСНОВНАЯ ЛОГИКА ========================
   */
 
+  // *** Переменная, определяющая язык Локали браузера ***
+  const browserLanguage = navigator.language;
+
+  /*
+  *** Условия определения, на каком языке будет выведено
+  *** валидационное сообщение (по умолчанию — на Русском)
+  */
+  let phoneValidityMessage = EMPTY;
+  switch (true) {
+    case browserLanguage === LanguageAbbr.RU || browserLanguage === LanguageAbbr.ru_RU:
+      phoneValidityMessage = ValidityMessage.PHONE_RU;
+      break;
+
+    case browserLanguage === LanguageAbbr.EN || browserLanguage === LanguageAbbr.en_US || browserLanguage === LanguageAbbr.en_GB:
+      phoneValidityMessage = ValidityMessage.PHONE_EN;
+      break;
+
+    default:
+      phoneValidityMessage = ValidityMessage.PHONE_RU;
+  }
+
+
   /*
   *** Ф-ция для обработчика события ИЗМЕНЕНИЯ содержимого
   *** поля ввода номера телефона
@@ -187,7 +224,6 @@ window.maskForInputs = (() => {
   const onInputChange = (inputElement) => {
     const regExpForInput = /^\d$/;
     const valueOfInput = inputElement.value;
-    const inputValidityMessage = 'The phone number is incomplete. Please enter your phone number.';
 
     // --- Счётчик цифровых символов ---
     let truePhoneLength = 0;
@@ -210,7 +246,7 @@ window.maskForInputs = (() => {
     *** ТОГДА выведется валидационное сообщение
     */
     if (truePhoneLength < MIN_PHONE_LENGHT) {
-      inputElement.setCustomValidity(inputValidityMessage);
+      inputElement.setCustomValidity(phoneValidityMessage);
     } else {
       inputElement.setCustomValidity(EMPTY);
     }
